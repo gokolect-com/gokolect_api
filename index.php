584eb7c5cd8aaae18dd56d12f48379c705ac4fa3
Samuel Joseph Tamunobarasinipiri <joseph.samuel@cinfores.com>
2023-03-06 11:33:42 +0100

    Update the index.php

--- index.php
+++ index.php
@@ -19,24 +19,13 @@ ini_set('display_errors', 1);
  // Allow from any origin
 // if (isset($_SERVER['HTTP_ORIGIN'])) {
     header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Expose-Headers: Content-Length, X-JSON");
     header("Content-Type: application/json; charset=UTF-8");
     header('Access-Control-Allow-Credentials: true');
     header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-    header("Access-Control-Max-Age: 3600");
-    header("Access-Control-Allow-Headers:Origin, Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, accept, ");
     header("Access-Control-Allow-Headers:Origin, Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, accept, X-Auth-Token");
     header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
 // }
 
-// Access-Control headers are received during OPTIONS requests
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS' || $_SERVER['REQUEST_METHOD'] == 'POST' || $_SERVER['REQUEST_METHOD'] == 'GET') {
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-}
 
 if (isset($_POST['action']) || isset($_GET['action'])) {        
     include_once __DIR__ ."/web/app/Route/route_index.php";    
