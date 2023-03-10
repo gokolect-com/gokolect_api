5e505e658e15045aef1df5d2d3e55299714326fb
Samuel Joseph Tamunobarasinipiri <joseph.samuel@cinfores.com>
2023-03-10 14:22:23 +0100

    Debugging Steps 1

--- web/app/Api/Utility.php
+++ web/app/Api/Utility.php
@@ -66,15 +66,12 @@ class Utility
                 'allow_self_signed' => true
                 )
             );                                         //Send using SMTP
-            // $mail->Host       = 'ssl://smtp.gmail.com';                 //Set the SMTP server to send through
-            // $mail->Host = 'smtp.zoho.com:465';                     //Set the SMTP server to send through
             // $mail->Host = 'smtp.hostinger.com';                     //Set the SMTP server to send through
             $mail->Host = 'ssl://smtp.titan.email';                     //Set the SMTP server to send through
             $mail->SMTPAuth = true;                                   //Enable SMTP authentication
-            // $mail->Username = 'josephsamuelw1@zohomail.com';                     //SMTP username
             $mail->Username = 'gokolect_info@bootqlass.com';                     //SMTP username
             $mail->Password = 'jos57atg@0806';                               //SMTP password
-            // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTP;            //Enable implicit TLS encryption
             $mail->SMTPSecure = "ssl";            //Enable implicit TLS encryption
             $mail->Port = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`           
 
